b7373016122526a82a9aa9b5c026d93258dc4bdd
Abdel-Rahman Muhammed <39808944+abdelrahmanMA@users.noreply.github.com>
2018-06-20 20:19:47 +0200

    Create compare.py
    compare scripts that compares the databases produced by gffutils for the input samples

---
+++ compare.py
@@ -0,0 +1,192 @@
+import gffutils
+
+class Feature(gffutils.Feature):
+
+    def __init__(self, feature):
+        gffutils.Feature.__init__(self, feature.seqid, feature.source, feature.featuretype,
+                                  feature.start, feature.end, feature.score, feature.strand, feature.frame,
+                                  feature.attributes, feature.extra, feature.bin, feature.id, feature.dialect,
+                                  feature.file_order, feature.keep_order, feature.sort_attribute_values)
+        self.size = feature.end - feature.start + 1
+
+
+class Exon(Feature):
+
+    type_ = None
+    cat_ = None
+
+    def __init__(self, feature, transcript):
+        Feature.__init__(self, feature)
+        self.transcript = transcript
+        
+
+    def set_exon_cat(self):
+        if self.start == self.transcript.start:
+            self.cat_ = 'first'
+        elif self.end == self.transcript.end:
+            self.cat_ = 'last'
+        else:
+            self.cat_ = 'mid'
+
+    @staticmethod
+    def set_exon_type(listofexons):
+        max_first_exon = 0
+        max_last_exon = 0
+        longest_first_exon_id = None
+        longest_last_exon_id = None
+
+        for exon_ in listofexons:
+            if exon_.cat_ == 'first':
+                if exon_.size > max_first_exon:
+                    max_first_exon = exon_.size
+                    longest_first_exon_id = exon_.id
+            elif exon_.cat_ == 'last':
+                if exon_.size > max_last_exon:
+                    max_last_exon = exon_.size
+                    longest_last_exon_id = exon_.id
+
+        for exon_ in listofexons:
+            if exon_.cat_ == 'first':
+                if longest_first_exon_id == exon_.id:
+                    exon_.type_ = 'Type 1'
+                else:
+                    exon_.type_ = 'Type 1/2'
+            elif exon_.cat_ == 'last':
+                if longest_last_exon_id == exon_.id:
+                    exon_.type_ = 'Type 2'
+                else:
+                    exon_.type_ = 'Type 2/2'
+
+        for cexon in listofexons:
+            if cexon.cat_ == 'mid':
+                for nexon in listofexons:
+                    if cexon.id != nexon.id:
+                        if nexon.cat_ == 'first':
+                            if Exon.match_exons(cexon, nexon)[0]:
+                                cexon.type_ = 'Type 4'
+                                break
+                        elif nexon.cat_ == 'last':
+                            if Exon.match_exons(cexon, nexon)[0]:
+                                cexon.type_ = 'Type 5'
+                                break
+                if cexon.type_ is None:
+                    cexon.type_ = 'Type 3'
+
+    @staticmethod
+    def match_exons(exon1, exon2):
+        if exon1.start > exon2.start:
+            temp = exon1
+            exon1 = exon2
+            exon2 = temp
+        if exon1.end - exon2.start > 0:
+            return [True, exon1.end - exon2.start]
+        return [False, None]
+
+
+class Parents:
+    def __init__(self, db):
+        self.db = db
+
+    def gene(self, feature):
+        return next(self.db.parents(feature.id, featuretype='gene'))
+
+    def transcript(self, feature):
+        return next(self.db.parents(feature.id, featuretype='transcript'))
+
+    def transcript(self, feature):
+        return next(self.db.parents(feature, featuretype='transcript'))
+
+    def transcriptstart(self, feature):
+        return next(self.db.parents(feature.id, featuretype='transcript')).start
+
+    def transcriptend(self, feature):
+        return next(self.db.parents(feature.id, featuretype='transcript')).end
+
+
+def find_best_match(asm_features, ref_features):
+    best_matches = {}
+    ref_matches = {}
+    asm_matches = {}
+    start = 0
+    sz = len(asm_features)
+    for index in range(sz):
+        asm_feature = Feature(asm_features[index])
+        matched_ref_feature, max_basepair = None, 0
+        for refind in range(start, len(ref_features)):
+            ref_feature = Feature(ref_features[refind])
+            if asm_feature.end < ref_feature.start:
+                break
+            if asm_feature.start > ref_feature.end:
+                continue
+            if index != sz - 1:
+                if ref_feature.end < asm_features[index + 1].start:
+                    start = refind
+            overlap, basepair = Exon.match_exons(asm_feature, ref_feature)
+            if overlap and basepair > max_basepair:
+                matched_ref_feature = Feature(ref_feature)
+                max_basepair = basepair
+            elif overlap and basepair == max_basepair and max_basepair != 0:
+                if ref_feature.size < matched_ref_feature.size:
+                    matched_ref_feature = Feature(ref_feature)
+            if overlap:
+                if matched_ref_feature.id not in ref_matches.keys():
+                    ref_matches[matched_ref_feature.id] = basepair
+                else:
+                    ref_matches[matched_ref_feature.id] = max(ref_matches[matched_ref_feature.id], basepair)
+                if asm_feature.id not in asm_matches.keys():
+                    asm_matches[asm_feature.id] = basepair
+                else:
+                    asm_matches[asm_feature.id] = max(asm_matches[asm_feature.id], basepair)
+
+        if matched_ref_feature is not None:
+            if matched_ref_feature.id not in best_matches.keys():
+                best_matches[matched_ref_feature.id] = [(asm_feature.id, basepair)]
+            else:
+                best_matches[matched_ref_feature.id].append((asm_feature.id, basepair))
+    return [best_matches,ref_matches,asm_matches]
+
+
+refdb = gffutils.FeatureDB("ref_sample.gtf.db")
+ref_exons = list(refdb.features_of_type('exon', order_by='start'))
+
+
+asmdb = gffutils.FeatureDB("poly_brain_scallop_merged_sample.gtf.db")
+asm_exons = list(asmdb.features_of_type('exon', order_by='start'))
+
+ref_genes = refdb.features_of_type('gene', order_by='start')
+ref_parents = Parents(refdb)
+
+best_match,refmatches,asmmathes = find_best_match(asm_exons, ref_exons)
+
+asm_parents = Parents(asmdb)
+
+for gene in ref_genes:
+    refexons = []
+    child_exons = refdb.children(gene.id, featuretype='exon')
+    for exon in child_exons:
+        refexons.append(Exon(exon, ref_parents.transcript(exon)))
+        refexons[-1].set_exon_cat()
+    Exon.set_exon_type(refexons)
+    with open("result.txt", 'a') as file_:
+        for exon in refexons:
+            file_.write("-ref {} \t {} \t {} \t {} \t {} \t {} \t {}\n"
+                        .format(ref_parents.gene(exon).id, exon.transcript.id, exon.id, exon.start,
+                                exon.end, exon.size, exon.type_))
+            if exon.id in best_match.keys():
+                for match, bp in best_match[exon.id]:
+                    asm_exon = Exon(asmdb[match], asm_parents.transcript(match))
+                    if bp == (asm_exon.size + exon.size)/2:
+                        relation = 'Perfect'
+                    else:
+                        relation = 'Partial'
+                    file_.write("*asm {} \t {} \t {} \t {} \t {} \t {} \t {}\n"
+                                .format(asm_parents.gene(asm_exon).id, asm_parents.transcript(asm_exon).id, asm_exon.id,
+                                        asm_exon.start, asm_exon.end, asm_exon.size, relation))
+with open("result.txt", 'a') as file_:
+    file_.write("\n--------------------------------------------------------\n")
+    for asmexon in asm_exons:
+        if asmexon.id not in asmmathes.keys():
+            asmexon = Exon(asmexon,asm_parents.transcript(asmexon))
+            file_.write("**** {} \t {} \t {} \t {} \t {} \t {} \t {}\n"
+                        .format(asm_parents.gene(asmexon).id, asm_parents.transcript(asmexon).id, asmexon.id,
+                                asmexon.start, asmexon.end, asmexon.size, "novel"))
