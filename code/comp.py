9859d5bfaee045c1bbea5c6664e38d260bfb2e48
Abdel-Rahman Muhammed <39808944+abdelrahmanMA@users.noreply.github.com>
2019-03-30 13:48:17 +0200

    Update comp.py

--- code/comp.py
+++ code/comp.py
@@ -143,16 +143,14 @@ def argument_parser():
 
     parser = ArgumentParser()
 
-    parser.add_argument("-r", dest="refgtf", help='''a set of known mRNAs to use as a reference for assessing 
-       the accuracy of mRNAs or gene models given in <input.gtf>''',
+    parser.add_argument("-r", dest="refgtf", help='''“reference” annotation GTF file. Each sample is matched against this file, and sample isoforms are tagged as overlapping, matching, or novel where appropriate. <input.gtf>''',
                         metavar="<reference_mrna>", nargs='?', required=True)
 
     parser.add_argument("-i", help='''provide a text file with a list of GTF files to process instead
        of expecting them as command line arguments (useful when a large number
        of GTF files should be processed)''', metavar="<input_list>", nargs='?')
 
-    parser.add_argument('input_annotations', help='''a set of known mRNAs to use as a reference for assessing 
-       the accuracy of mRNAs or gene models given in <input.gtf>''', metavar='<input.gtf>', nargs='*')
+    parser.add_argument('input_annotations', help='''input GTF files to be matched against the reference GTF<input.gtf>''', metavar='<input.gtf>', nargs='*')
 
     parser.add_argument('-th', help='a basepair threshold for exon matching',
                         metavar='basepair threshold', type=int, nargs=1, default=[0])
