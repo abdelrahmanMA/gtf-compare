4b36915d716a4e00a6f4e03a90fcdce04f468051
abdelrahmanMA <bme.abdelrahmanmuhammed@gmail.com>
2019-06-27 05:54:52 +0200

    notes created

---
+++ gtfcompare/notes.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+def get_notes():
+    NOTES = ['-',
+         'A match was found on the unknown strand of the reference',
+         'A match was found on the opposite strand on the reference',
+         'A match was found on the + and - strands of the reference query\'s strand is unknown',
+         'More than one best match was found all on the same strand same as query strand',
+         'More than one best match was found on the unknown strand of reference',
+         'More than one best match was found all on the same strand opposite of query strand',
+         'More than one best match was found on both strands of reference query\'s strand is unknown',
+         '']
+    return NOTES
