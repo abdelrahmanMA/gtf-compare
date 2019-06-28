f8b5d7c0026b30e66578496d56da47f517b1cad1
abdelrahmanMA <bme.abdelrahmanmuhammed@gmail.com>
2019-06-28 17:49:01 +0200

    test write exon

--- gtfcompare/compare.py
+++ gtfcompare/compare.py
@@ -5,6 +5,7 @@ import time
 from argument_parser import argument_parser
 from to_gtftree import to_gtftree
 from interval_matching import match_intervals
+from writers import write_exons
 
 reference, input_files = argument_parser()
 
@@ -13,11 +14,16 @@ ref_chrom_dict, ref_introns = to_gtftree(reference, 'ref')
 time_end = time.time()
 print("ref", time_end - time_begin)
 for input_file in input_files:
+    shortname = input_file.split('/')[-1]
     time_begin = time.time()
-    quer_chrom_dict, quer_introns = to_gtftree(input_file, input_file.split('/')[-1])
+    quer_chrom_dict, quer_introns = to_gtftree(input_file, shortname)
     time_end = time.time()
-    print(input_file.split('/')[-1], time_end - time_begin)
+    print(shortname, time_end - time_begin)
     time_begin = time.time()
     match_intervals(quer_chrom_dict, ref_chrom_dict)
     time_end = time.time()
     print('match intervals', time_end - time_begin)
+    time_begin = time.time()
+    write_exons(shortname, quer_chrom_dict)
+    time_end = time.time()
+    print('write dotexon', time_end - time_begin)
